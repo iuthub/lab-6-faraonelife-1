6fa5a73dd2025a6610c17a194ccdd86d43202001
Farxod Khaitbaev <72006448+faraonelife@users.noreply.github.com>
2021-05-23 12:09:06 +0500

    done

--- webpage/regex_valid_form.php
+++ webpage/regex_valid_form.php
@@ -1,24 +1,24 @@
 <?php
 
-	$pattern="";
-	$text="";
-	$replaceText="";
-	$replacedText="";
+$pattern="";
+$text="";
+$replaceText="";
+$replacedText="";
 
-	$match="Not checked yet.";
+$match="Not checked yet.";
 
 if ($_SERVER["REQUEST_METHOD"]=="POST") {
-	$pattern=$_POST["pattern"];
-	$text=$_POST["text"];
-	$replaceText=$_POST["replaceText"];
+    $pattern=$_POST["pattern"];
+    $text=$_POST["text"];
+    $replaceText=$_POST["replaceText"];
 
-	$replacedText=preg_replace($pattern, $replaceText, $text);
+    $replacedText=preg_replace($pattern, $replaceText, $text);
 
-	if(preg_match($pattern, $text)) {
-						$match="Match!";
-					} else {
-						$match="Does not match!";
-					}
+    if(preg_match($pattern, $text)) {
+        $match="Cool !";
+    } else {
+        $match="Does not match!";
+    }
 }
 
 ?>
@@ -27,31 +27,31 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Valid Form</title>
+    <meta charset="UTF-8">
+    <title>Valid Form</title>
 </head>
 <body>
-	<form action="regex_valid_form.php" method="post">
-		<dl>
-			<dt>Pattern</dt>
-			<dd><input type="text" name="pattern" value="<?= $pattern ?>"></dd>
+<form action="regex_valid_form.php" method="post">
+    <dl>
+        <dt>Pattern</dt>
+        <dd><input type="text" name="pattern" value="<?= $pattern ?>"></dd>
 
-			<dt>Text</dt>
-			<dd><input type="text" name="text" value="<?= $text ?>"></dd>
+        <dt>Text</dt>
+        <dd><input type="text" name="text" value="<?= $text ?>"></dd>
 
-			<dt>Replace Text</dt>
-			<dd><input type="text" name="replaceText" value="<?= $replaceText ?>"></dd>
+        <dt>Replace Text</dt>
+        <dd><input type="text" name="replaceText" value="<?= $replaceText ?>"></dd>
 
-			<dt>Output Text</dt>
-			<dd><?=	$match ?></dd>
+        <dt>Output Text</dt>
+        <dd><?=	$match ?></dd>
 
-			<dt>Replaced Text</dt>
-			<dd> <code><?=	$replacedText ?></code></dd>
+        <dt>Replaced Text</dt>
+        <dd> <code><?=	$replacedText ?></code></dd>
 
-			<dt>&nbsp;</dt>
-			<dd><input type="submit" value="Check"></dd>
-		</dl>
+        <dt>&nbsp;</dt>
+        <dd><input type="submit" value="Check"></dd>
+    </dl>
 
-	</form>
+</form>
 </body>
 </html>
